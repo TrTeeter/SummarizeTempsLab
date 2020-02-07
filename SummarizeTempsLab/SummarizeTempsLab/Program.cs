5cffb29f66d3ab04fd08d55f2537cca98725370b
TrTeeter <59981794+TrTeeter@users.noreply.github.com>
2020-02-07 00:52:18 -0600

    Update Program.cs
    Works like a dream as long as the average is right

--- SummarizeTempsLab/SummarizeTempsLab/Program.cs
+++ SummarizeTempsLab/SummarizeTempsLab/Program.cs
@@ -7,95 +7,109 @@ namespace SummarizeTempsLab
     {
         static void Main(string[] args)
         {
-
-
-            int tempthresh, temp,avg;
+            Console.WriteLine("***Average Temperature**");
             
-           
+            int tempthresh, temp, avg;
+
+
             string input = ("");
             string filepath = ("");
             string line = "";
 
-
-            Console.WriteLine("***Average Temperature**");
             Console.WriteLine("Enter File Name");
-
             filepath = Console.ReadLine();
 
-            if (File.Exists(filepath))
+            bool Again = true;
+            while (Again)
             {
-                Console.WriteLine("Enter Temperature Threshold");
-                input = Console.ReadLine();
-                tempthresh = int.Parse(input);
+
+                
 
 
-                using (StreamReader sr = File.OpenText(filepath))
+                if (File.Exists(filepath))
                 {
-                    Console.WriteLine("Temperature above Threshhold:");
+                    Console.WriteLine("Enter Temperature Threshold");
+                    input = Console.ReadLine();
+                    tempthresh = int.Parse(input);
 
-                    while ((line = sr.ReadLine()) != null)
+
+                    using (StreamReader sr = File.OpenText(filepath))
                     {
-                        temp = int.Parse(line);
+                        Console.WriteLine("Temperature above Threshhold:");
 
-
-
-
-                        if (temp >= tempthresh)
+                        while ((line = sr.ReadLine()) != null)
                         {
-                            
-                            Console.WriteLine(temp);
-
-                        }
-
-                    }
-
-                   
-
-                }
-                using (StreamReader sr = File.OpenText(filepath))
-                {
-                    Console.WriteLine("Temperature at or Below Threshhold:");
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        temp = int.Parse(line);
+                            temp = int.Parse(line);
 
 
 
 
-                        if (temp <= tempthresh)
-                        {
-                           
-                            Console.WriteLine(temp);
+                            if (temp >= tempthresh)
+                            {
+
+                                Console.WriteLine(temp);
+
+                            }
 
                         }
 
 
+
                     }
-                }
-                using (StreamReader sr = File.OpenText(filepath))
-                {
-                    int sum = 0;
-                    Console.WriteLine("Average Temperature:");
-                    while ((line = sr.ReadLine()) != null)
+                    using (StreamReader sr = File.OpenText(filepath))
                     {
-                        temp = int.Parse(line);
-                        
-                        sum = temp + sum;
+                        Console.WriteLine("Temperature at or Below Threshhold:");
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            temp = int.Parse(line);
+
+
+
+
+                            if (temp <= tempthresh)
+                            {
+
+                                Console.WriteLine(temp);
+
+                            }
+
+
+                        }
                     }
-                    avg = sum / 365;
-                    Console.WriteLine(avg);
-                   
+                    using (StreamReader sr = File.OpenText(filepath))
+                    {
+                        int sum = 0;
+                        Console.WriteLine("Average Temperature:");
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            temp = int.Parse(line);
+
+                            sum = temp + sum;
+                        }
+                        avg = sum / 365;
+                        Console.WriteLine(avg);
+
+                    }
+                    Console.WriteLine("Would You Like to Change Your Temperature? yes or no?");
+                    input = Console.ReadLine();
+
+                    if (input == "no")
+                    {
+                        Again = false;
+                        Console.WriteLine("Thank You!");
+                    }
                 }
+                else
+                {
+                    Console.WriteLine("File Does Not Exist " +
+                        "Please Try Again");
+                   
+                    filepath = Console.ReadLine();
+                }
+               
 
-
-                        Console.WriteLine("Thank You! Have a Nice Day");
             }
-            else
-            {
-                Console.WriteLine("File Does Not Exist " +
-                    "Please Try Again");
-                Console.ReadLine();
-            }
+            
         }
     }
 }
